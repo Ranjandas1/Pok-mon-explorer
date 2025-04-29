fad9a49fe08086f378cc7af3a1bd2a9026a267d9
Ranjan Das <dranjan751@gmail.com>
2025-04-29 12:20:02 +0530

    code committed for offline show the image using pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,48 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["pokeball-pokemon-svgrepo-com.svg", "robots.txt"],
+      manifest: {
+        name: "Pokémon Explorer",
+        short_name: "PokeExplorer",
+        description: "Explore Pokémon with ease",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) =>
+              request.destination === "document" ||
+              request.destination === "script" ||
+              request.destination === "style",
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "app-cache",
+            },
+          },
+        ],
+      },
+    }),
+  ],
 });
